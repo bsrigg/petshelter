963aea578acf6e58aeadf2c66ac3163283f8ad18
Barry S Rigg <barrysrigg@gmail.com>
2019-12-17 11:16:24 -0500

    Update for Security issues

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
+import '../build/static/img/heart.png'
 import "react-router";
 import { BrowserRouter, Route } from 'react-router-dom';
 import DashBoard from './components/DashBoard';
